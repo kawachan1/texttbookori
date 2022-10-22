1291b51c7f631f70bd8fc5e139388fac5cba8aca
admin <admin@example.com>
2022-10-22 15:31:00 +0900

    l

--- config/settings.py
+++ config/settings.py
@@ -136,9 +136,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+# STATICFILES_DIRS = (
+#  os.path.join(BASE_DIR, "static"),
+# )
 # if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
 # in your application directory on Render.
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
