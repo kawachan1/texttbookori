0b6ef0fb7d19387d9c004815e90e9e82cf3921b0
admin <admin@example.com>
2022-10-22 15:20:31 +0900

    l

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 #SECRET_KEY = 'e$zu5+$ab@)p&x81otyxz^plbzn-iu1mw@z5a9pggrh(ggoh&m'
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+#DEBUG = False
+DEBUG = True
 #DEBUG = 'RENDER' not in os.environ
 
 
