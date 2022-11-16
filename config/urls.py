eaf145fb507966ebb73d92f015c1b9e4f6a3c58e
kawahata1 <s1f102002200@iniad.org>
2022-11-16 15:21:00 +0900

    背景

--- config/urls.py
+++ config/urls.py
@@ -20,15 +20,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
 
-from django.conf.urls import url
+#from django.conf.urls import url
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('text/',include('text.urls')),
     path('', include('text.urls')),
-    url(r'^media/(?P<path>.*)$', serve,
-        {'document_root': settings.MEDIA_ROOT}),
+    path(r'^media/(?P<path>.*)$', serve,
+         {'document_root': settings.MEDIA_ROOT}),
 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
